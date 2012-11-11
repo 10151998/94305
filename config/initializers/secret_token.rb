128c4dc6b983754a3bc6df0dcbdfcba26b05ec5a
10151998 <ctango89@alumni.stanford.edu>
2012-11-11 09:06:12 -0500

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+APIDemo::Application.config.secret_token = '5e64657b79794c34818c8ef47ca974a7f5a460372371dc24f5a29057b0ba6ed763112bacaf059deb8251361418dfd1dea5cfd1f9abbb4da7f779ce46046addf8'
